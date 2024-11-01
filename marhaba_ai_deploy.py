87528a6334c7decb12d0fe3221bd4703f273bf59
Rahul P R <rahulpodi@gmail.com>
2024-11-01 16:19:08 +0530

    Update marhaba_ai_deploy.py
    recipe photo title

--- marhaba_ai_deploy.py
+++ marhaba_ai_deploy.py
@@ -146,7 +146,7 @@ else:
         st.markdown(recipe_response.choices[0].message.content)
         if len(recipe_response.choices[0].message.content)>0:
             recipe_prompt = "Create a photo of the food item that is described here: "+recipe_response.choices[0].message.content
-            st.caption("Here is a photo of the recipe as well!")
+            st.title("Here is a photo of the recipe as well!")
             client = OpenAI()
             recipe_response = client.images.generate(
                 model="dall-e-3",
