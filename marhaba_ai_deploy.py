fac0db9097a25e5f4ec41c61f49667a582ea736b
Rahul P R <rahulpodi@gmail.com>
2025-01-13 23:56:54 +0530

    Receipt Verification

--- marhaba_ai_deploy.py
+++ marhaba_ai_deploy.py
@@ -9,6 +9,8 @@ import streamlit as st
 import ollama
 from openai import OpenAI
 import base64
+import pandas as pd
+import re
 
 ### UI Functions
 def stream_data(text):
@@ -27,7 +29,7 @@ os.environ['OPENAI_API_KEY'] = st.secrets["openai_secret_key"]
 
 with st.sidebar:
     st.image(marhabaailogo)
-    listofproducts = st.selectbox("Choose a product for demo:",("-","Adengappa kadhaigal!","Back to school!","Ramadan Campaign!","Ramadan Campaign! (Arabic)","Create your Ramadan recipe!","Spice up your dream moment!"),)
+    listofproducts = st.selectbox("Choose a product for demo:",("-","Adengappa kadhaigal!","Back to school!","Ramadan Campaign!","Ramadan Campaign! (Arabic)","Create your Ramadan recipe!","Spice up your dream moment!","Receipt Verification"),)
     if listofproducts == "-": choicetext = ""
     elif listofproducts == "Adengappa kadhaigal!": choicetext = "Scan your household item , turn them into stories and become your Kid's favourite Storyteller!"
     elif listofproducts == "Back to school!": choicetext = '''IMAGINE & CREATE!  
@@ -38,6 +40,7 @@ with st.sidebar:
     إعداد إفطار أحلامك باستخدام الذكاء الاصطناعي'''
     elif listofproducts == "Spice up your dream moment!": choicetext = '''IMAGINE & CREATE!  
     Your Dream Moment with AI'''
+    elif listofproducts == "Receipt Verification": choicetext = "Handling hundreds of receipts manually, Put AI to work!"
     else: choicetext = '''30 days. 30 inspirational  
     Recipes for Iftar with AI'''
     st.markdown(choicetext)
@@ -159,6 +162,86 @@ elif listofproducts == "Spice up your dream moment!":
             st.image(image_url)
         else:
             st.write("The prompt should contain the word 'lays'or 'Lays' as the campaign is regarding the same!")
+            
+# receipt verification
+elif listofproducts == "Receipt Verification":
+    st.title("AI Receipt Verification")
+    invoice_files = st.file_uploader("Upload your receipts", type=['png','jpg','jpeg'], accept_multiple_files=True)
+    
+    if (invoice_files is not None):
+        
+        invdf = pd.DataFrame(columns=['File Name','Product Found','Date of Invoice','Total Amount for Product','Second Iteration'])
+        count = 0
+        for fileval in invoice_files:
+            image_bytes_invoice = fileval.getvalue()
+            image_base64 = base64.b64encode(image_bytes_invoice).decode('utf-8')
+            invdf.loc[count,'File Name'] = fileval.name
+            client = OpenAI()
+            
+            # Extracting
+            response = client.chat.completions.create(
+                model='gpt-4o',
+                messages=[
+                    {
+                        "role":"user",
+                        "content":[
+                            {
+                                "type":"text",
+                                "text": "From the invoice/bill extract date of invoice(if it is not available, the output in the mentioned format should just be None. The year cannot be greater than 2024 or 2025. Understand the extracted date with this context as well), check if the product 'persil' or 'prsl' is found (yes/no) response (note: it could be in arabic as well in this way: برسيل . Consider only english if oth are present), if it is present then extract only total amount of all 'persil' or 'prsl' products without any currency denomination. The output representation has to always be in the following format (including commas and colons): 'Product Found: Only Yes/No, Date of Invoice: (in DD-MM-YYYY format), Total amount for product:'",
+                            },
+                            {
+                                "type":"image_url",
+                                "image_url":{"url": f"data:image/jpeg;base64,{image_base64}"},
+                            },
+                        ],
+                    }
+                ],
+                temperature=0.5,
+            )
+            response_text = response.choices[0].message.content
+            invdf.loc[count,'Product Found'] = response_text.split(',')[0].split(':')[1].strip()
+            invdf.loc[count,'Date of Invoice'] = response_text.split(',')[1].split(':')[1].strip()
+            invdf.loc[count,'Total Amount for Product'] = response_text.split(',')[2].split(':')[1].strip()
+            
+            # Second Extraction - to counter amount mismatches
+            checkresponse = client.chat.completions.create(
+                model='gpt-4o',
+                messages=[
+                    {
+                        "role":"user",
+                        "content":[
+                            {
+                                "type":"text",
+                                "text": "Print the amount corresponding to persil or prsl products (it can be in arabic as well in this way: برسيل . Consider only english if both are present) without any currency denomination. Don't total the amounts, just the raw values would suffice. Also a thing to note: don't get confused with unit price and the final purchase price. For e.g., let's say 27.95 is the unit price but since two units were purchased the final purchase will be 55.90. The required value in such cases is 55.90.",
+                            },
+                            {
+                                "type":"image_url",
+                                "image_url":{"url": f"data:image/jpeg;base64,{image_base64}"},
+                            },
+                        ],
+                    }
+                ],
+                temperature=0.5,
+            )
+            checkresponse_text = checkresponse.choices[0].message.content
+            invdf.loc[count,'Second Iteration'] = checkresponse_text
+            count+=1
+    
+    # Processing the combined dataset
+    invdf['Total Amount for Product'] = invdf['Total Amount for Product'].apply(lambda x: re.sub(r'[^0-9.]','',x).strip('.'))
+    invdf['Total Amount for Product'] = pd.to_numeric(invdf['Total Amount for Product'])
+    
+    # Creating another second iteration column
+    invdf['Second Iteration1'] = invdf['Second Iteration1'].apply(lambda x: sum(float(num) for num in re.findall(r'\d+\.\d+',x)))
+    invdf['Second Iteration1'] = pd.to_numeric(invdf['Second Iteration1'])
+    
+    for row in range(0,invdf.shape[0]):
+        if invdf.loc[row,'Total Amount for Product'] != invdf.loc[row,'Second Iteration1']:
+            invdf.loc[row,'Total Amount for Product'] = invdf.loc[row,'Second Iteration1']
+    
+    # Selecting only the required columns
+    finalinvfdf = invdf[['File Name','Product Found','Date of Invoice','Total Amount for Product']]
+    print(finalinvfdf)
 else:
     st.title("Create your recipe with AI!")
     img_file_buffer_recipe = st.file_uploader("Upload a picture of a food item!",type=['png','jpg','jpeg'],accept_multiple_files=False)
