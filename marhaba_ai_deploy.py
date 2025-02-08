9f35e9b9671a49f0909a7834f1de5c3c482cba07
Rahul P R <rahulpodi@gmail.com>
2025-02-08 23:18:50 +0530

    o1 mini instead of o1

--- marhaba_ai_deploy.py
+++ marhaba_ai_deploy.py
@@ -174,7 +174,7 @@ elif listofproducts == "AI Hair Expert":
         image_base64_hair = base64.b64encode(image_bytes_hair).decode('utf-8')
         client = OpenAI()
         hair_response = client.chat.completions.create(
-              model="o1",
+              model="o1-mini",
               messages=[
                 {
                   "role": "user",
