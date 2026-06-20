9972af7a8c684aa135fa74d008c72c1c8e995404
Rahul P R <rahulpodi@gmail.com>
2026-06-21 00:35:10 +0530

    Update marhaba_ai_deploy.py
    receiving in base64 instead of URL for gpt-image-2

--- marhaba_ai_deploy.py
+++ marhaba_ai_deploy.py
@@ -97,8 +97,10 @@ elif listofproducts=="Back to school!":
                 quality="high",
                 n=1,
                 )
-            image_url = school_response.data[0].url
-            st.image(image_url)
+            #image_url = school_response.data[0].url
+            #st.image(image_url)
+            image_base64 = school_response.data[0].b64_json
+            image_bytes = base64.b64decode(image_base64)
         else:
             st.write("The prompt should contain the word 'school' as the campaign is about 'Back to school!'")
 
